4f53c151e7bd4a9086801997e86bd3409a2d0552
ashok <ashokurlana@gmail.com>
2018-12-30 10:10:37 +0530

    seraching

---
+++ searching.py
@@ -0,0 +1,26 @@
+import urllib.request
+import re
+baseurl="https://www.bing.com/search?q="
+oquery="what is the capital of india"
+squery= oquery.replace(' ', '+')
+query = baseurl+squery
+#print(query)
+url= urllib.request.urlopen(query)
+data=url.read()
+data=str(data)
+#regex= "href=\".*?\""
+#p=re.compile(regex)
+#s= re.findall(r'href=\".*?\"', data)
+s=re.findall(r'href=\"https://.*?\"',data)
+links=[]
+i=0;
+while(i<len(s)):
+    string=s[i]
+    links.append(string[6:-1])
+    i=i+1
+print(links)
+
+
+    print(link)
+#newdata= re.search(regex, data)
+#print(newdata)
